648c2d7b0ba365ec0396197a92e0802ef86203ac
sm-karthi <karthikeyan24022007@gmail.com>
2025-06-18 22:40:15 +0530

    Finished the create product page UI

--- src/components/admin/ProductCreate.jsx
+++ src/components/admin/ProductCreate.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function ProductCreate() {
+
+  let [rows, setRows] = useState([true]);
+
+  let handleAddBtn = () => {
+    setRows([...rows, true]);
+  }
+
+  let handleCloseBtn = (index) => {
+    let updatedRow = [...rows];
+    updatedRow.splice(index, 1);
+    setRows(updatedRow)
+  }
+
   return (
     <div className='p-6 py-10'>
 
@@ -14,7 +27,7 @@ function ProductCreate() {
 
             <label className='font-semibold text-lg text-gray-700'>Title:</label>
 
-            <input type="text" name='title' className='md:w-lg mt-1 border px-3 py-2 rounded' />
+            <input type="text" name='title' className='md:w-lg mt-1 border px-3 py-2 rounded focus:outline-none focus:border-blue-500' />
 
             <span className='text-red-500'></span>
 
@@ -24,7 +37,7 @@ function ProductCreate() {
 
             <label className='font-semibold text-lg text-gray-700'>Image URL:</label>
 
-            <input type="text" name='title' className='md:w-lg mt-1 border px-3 py-2 rounded' />
+            <input type="text" name='title' className='md:w-lg mt-1 border px-3 py-2 rounded focus:outline-none focus:border-blue-500' />
 
             <span className='text-red-500'></span>
 
@@ -34,67 +47,76 @@ function ProductCreate() {
 
             <label className='font-semibold text-lg text-gray-700'>Description:</label>
 
-            <textarea className='md:w-lg mt-1 border px-3 py-2 rounded h-22'></textarea>
+            <textarea className='md:w-lg mt-1 border px-3 py-2 rounded h-22 focus:outline-none focus:border-blue-500'></textarea>
 
             <span className='text-red-500'></span>
 
           </div>
 
           <svg
-            className='cursor-pointer'
+            onClick={handleAddBtn}
+            className="cursor-pointer w-[50px] h-[50px] fill-blue-500 hover:fill-blue-700 drop-shadow-lg transition duration-200"
             xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            width={50}
-            height={50}
-            viewBox="0,0,256,256"
+            viewBox="0 0 256 256"
           >
-            <g
-              fill="#305aeb"
-
-            >
-              <g transform="scale(8.53333,8.53333)">
-                <path d="M15,3c-6.627,0 -12,5.373 -12,12c0,6.627 5.373,12 12,12c6.627,0 12,-5.373 12,-12c0,-6.627 -5.373,-12 -12,-12zM21,16h-5v5c0,0.553 -0.448,1 -1,1c-0.552,0 -1,-0.447 -1,-1v-5h-5c-0.552,0 -1,-0.447 -1,-1c0,-0.553 0.448,-1 1,-1h5v-5c0,-0.553 0.448,-1 1,-1c0.552,0 1,0.447 1,1v5h5c0.552,0 1,0.447 1,1c0,0.553 -0.448,1 -1,1z" />
-              </g>
+            <g transform="scale(8.53333,8.53333)">
+              <path d="M15,3c-6.627,0 -12,5.373 -12,12c0,6.627 5.373,12 12,12c6.627,0 12,-5.373 12,-12c0,-6.627 -5.373,-12 -12,-12zM21,16h-5v5c0,0.553 -0.448,1 -1,1c-0.552,0 -1,-0.447 -1,-1v-5h-5c-0.552,0 -1,-0.447 -1,-1c0,-0.553 0.448,-1 1,-1h5v-5c0,-0.553 0.448,-1 1,-1c0.552,0 1,0.447 1,1v5h5c0.552,0 1,0.447 1,1c0,0.553 -0.448,1 -1,1z" />
             </g>
           </svg>
 
-          <div className='flex flex-col md:flex-row space-y-3 md:space-y-0 justify-between'>
 
-            <div className='flex flex-col'>
+          {
+            rows.map((row, index) => (
 
-              <label className='font-semibold text-lg text-gray-700'>Key:</label>
+              <div className='flex relative flex-col md:flex-row space-y-3 md:space-y-0 justify-between'>
 
-              <input type="text" name='title' className='md:w-40 mt-1 border px-3 py-2 rounded' />
+                <div className='flex flex-col'>
 
-              <span className='text-red-500'></span>
+                  <label className='font-semibold text-lg text-gray-700'>Key:</label>
 
-            </div>
+                  <input type="text" name='title' className='md:w-40 mt-1 border px-3 py-2 rounded focus:outline-none focus:border-blue-500' />
 
+                  <span className='text-red-500'></span>
 
-            <div className='flex flex-col'>
-
-              <label className='font-semibold text-lg text-gray-700'>Value:</label>
-
-              <input type="text" name='title' className='md:w-40 mt-1 border px-3 py-2 rounded' />
-
-              <span className='text-red-500'></span>
-
-            </div>
-
-            <div className='flex flex-col'>
-
-              <label className='font-semibold text-lg text-gray-700'>Quantity:</label>
-
-              <input type="text" name='title' className='md:w-40 mt-1 border px-3 py-2 rounded' />
-
-              <span className='text-red-500'></span>
-
-            </div>
-
-          </div>
+                </div>
 
 
+                <div className='flex flex-col'>
+
+                  <label className='font-semibold text-lg text-gray-700'>Value:</label>
+
+                  <input type="text" name='title' className='md:w-40 mt-1 border px-3 py-2 rounded focus:outline-none focus:border-blue-500' />
+
+                  <span className='text-red-500'></span>
+
+                </div>
+
+                <div className='flex flex-col'>
+
+                  <label className='font-semibold text-lg text-gray-700'>Quantity:</label>
+
+                  <input type="text" name='title' className='md:w-40 mt-1 border px-3 py-2 rounded focus:outline-none focus:border-blue-500' />
+
+                  <span className='text-red-500'></span>
+
+                </div>
+
+                {
+                  rows.length > 1 ? (
+                    <button
+                    type='button'
+                    onClick={() => handleCloseBtn(index)}
+                    className='absolute top-1 right-0 bg-red-500 rounded-full w-6 h-6 text-white hover:bg-red-700 font-bold cursor-pointer shadow-md'>X</button>
+                  ) : null
+                }
+
+              </div>
+
+            ))
+          }
+
+
+          <button className='bg-blue-500 text-white px-3 py-1 rounded font-semibold hover:bg-blue-700 transition duration-150 shadow-md cursor-pointer'>Submit</button>
 
         </form>
 
